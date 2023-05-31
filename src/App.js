bce3471d776910fb2d0034bd8e9f50c76ce30e37
kukot <phucnn.work@gmail.com>
2023-05-31 23:20:43 +0800

    Parameterize component property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      date: new Date(2023, 5, 31),
+      name: 'Car Insurance',
+      amount: 'S$ 294.67'
+    },
+    {
+      id: 1,
+      date: new Date(2023, 4, 1),
+      name: 'Grocery shopping',
+      amount: 'S$ 50'
+    },
+    {
+      id: 1,
+      date: new Date(2023, 5, 12),
+      name: 'Mobile plan',
+      amount: 'S$ 10.00'
+    },
+
+  ]
+
   return (
     <div>
-      <ExpenseItem/>
-      <ExpenseItem/>
-      <ExpenseItem/>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          id={expense.id}
+          date={expense.date}
+          name={expense.name}
+          amount={expense.amount}>
+        </ExpenseItem>
+      ))
+
+      }
+
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-  return (
+
+function ExpenseItem(props) {
+    return (
     <div className="expense-item">
-      <div>May 25, 2023</div>
+      <input type='hidden' value={props.id}></input>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>S$294.67</div>
+        <h2>{props.name}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
       </div>
     </div>
   );
