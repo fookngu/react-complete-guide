a1d9de5606fb556c2bb926f459fb0a9c06b1fe44
Kevin Nguyen <kevin.nguyen@contour.network>
2023-07-18 21:02:02 +0800

    Introducing Card component as the wrapper

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card"
 import './Expenses.css'
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
@@ -13,7 +14,7 @@ function Expenses(props) {
           amount={expense.amount}
         ></ExpenseItem>
       ))}
-    </div>
+    </Card>
   );
 }
 
