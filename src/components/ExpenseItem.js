3a7155b59976dc8e3c26934dadb8e87a0e0b6fcb
kukot <phucnn.work@gmail.com>
2023-07-18 21:02:02 +0800

    Introducing Card component as the wrapper

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem({id, date, name, amount}) {
     return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <input type='hidden' value={id}></input>
       <ExpenseDate date = {date}></ExpenseDate>
       <div className='expense-item__description'>
         <h2>{name}</h2>
         <div className='expense-item__price'>{amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
