59a48e1005f22def8efd2ecf4a0ffd01221280f7
kukot <phucnn.work@gmail.com>
2023-07-18 21:45:19 +0800

    More details on what JSX does React.createElement as the old-school way of returning element

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,13 @@
+import React from 'react'
 import './Card.css'
 
 function Card(props) {
 
     const classes = 'card ' + props.className
+
+    // the following line would lead to the same result
+    // return React.createElement('div', {className: classes, children: props.children})
+
     return (
         <div className={classes}>
             {props.children}
