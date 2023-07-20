ea43f7d69c348e2106b5a0f14930173b055035c9
Kevin Nguyen <kevin.nguyen@contour.network>
2023-07-20 21:15:08 +0800

    button, click listener and working with state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 const ExpenseItem = ({id, date, name, amount}) => {
+
+    const [title, setTitle] = useState(name)
+
+    const changeTitleHandler = ()=> {
+      setTitle(name + " has changed")
+      console.log(title) // this still shows the old value. React does schedule a value update but not do it right away
+    }
     return (
     <Card className="expense-item">
       <input type='hidden' value={id}></input>
       <ExpenseDate date = {date}></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{name}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>{amount}</div>
       </div>
+      <button onClick={changeTitleHandler}>Change Title</button>
     </Card>
   );
 }
