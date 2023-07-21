8d8696a52a1cafca950ceba9239a4ec3c032473e
Kevin Nguyen <kevin.nguyen@contour.network>
2023-07-22 00:39:59 +0800

    Expense filter by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card"
-import './Expenses.css'
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState('2020')
+  const onYearSelectedHandler = (year) => {
+    setSelectedYear(year)
+    console.log(year)
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={selectedYear} onYearSelected={onYearSelectedHandler}/>
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
@@ -16,6 +25,6 @@ const Expenses = (props) => {
       ))}
     </Card>
   );
-}
+};
 
-export default Expenses
+export default Expenses;
