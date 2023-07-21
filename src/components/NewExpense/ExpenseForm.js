8b31c0c115955ce59cb14dc3c09f78e6cef92e2e
Kevin Nguyen <kevin.nguyen@contour.network>
2023-07-21 21:48:07 +0800

    single state approach with function to set user input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,48 +6,66 @@ const ExpenseForm = () => {
   // const [inputAmount, setInputAmount] = useState('')
   // const [inputDate, setInputDate] = useState('')
   const [userInput, setUserInput] = useState({
-    inputTitle: '',
-    inputAmount: '',
-    inputDate: ''
-  })
+    inputTitle: "",
+    inputAmount: "",
+    inputDate: "",
+  });
   const titleChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      inputTitle: event.target.value
-    })
-    console.log(userInput)
-  }
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        inputTitle: event.target.value,
+      };
+    });
+    console.log(userInput);
+  };
 
   const amountChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      inputAmount: event.target.value
-    })
-    console.log(userInput)
-  }
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        inputAmount: event.target.value,
+      };
+    });
+    console.log(userInput);
+  };
 
   const dateChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      inputDate: event.target.value
-    })
-    console.log(userInput)
-  }
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        inputDate: event.target.value,
+      };
+    });
+    console.log(userInput);
+  };
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" name="title" onChange={titleChangeHandler}/>
+          <input type="text" name="title" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" name="amount" onChange={amountChangeHandler}/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            name="amount"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" name="date" min="2023-07-01" max="2025-07-31" onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            name="date"
+            min="2023-07-01"
+            max="2025-07-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
