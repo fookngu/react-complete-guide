dcb20610d9250135a18cf4db273a9c44f967cf8a
Kevin Nguyen <kevin.nguyen@contour.network>
2023-07-22 13:09:21 +0800

    computed value as an alternative to state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [selectedYear, setSelectedYear] = useState('2020')
+  const [selectedYear, setSelectedYear] = useState("2020");
+  let excludedYear = "2019, 2021, 2022";
   const onYearSelectedHandler = (year) => {
-    setSelectedYear(year)
-    console.log(year)
+    setSelectedYear(year);
+  };
+
+  if (selectedYear === "2019") {
+    excludedYear = "2020, 2021, 2022";
+  } else if (selectedYear === "2020") {
+    excludedYear = "2019, 2021, 2022";
+  } else if (selectedYear === "2021") {
+    excludedYear = "2019, 2020, 2022";
+  } else {
+    excludedYear = "2019, 2020, 2021";
   }
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={selectedYear} onYearSelected={onYearSelectedHandler}/>
+      <ExpensesFilter
+        selected={selectedYear}
+        onYearSelected={onYearSelectedHandler}
+      />
+      <p style={{color: 'white'}}>Data for {excludedYear} is hidden</p>
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
